7bfe61e4dddf4790074fb1987bb4345cbd84b5ef
seunggod <kiss0703@nate.com>
2023-12-01 21:23:24 +0900

    commit myrepo

---
+++ advanced_first_project/src/main/java/com/tenco/team_two_flight_ticket/TeamTwoFlightTicketApplication.java
@@ -0,0 +1,13 @@
+package com.tenco.team_two_flight_ticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamTwoFlightTicketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamTwoFlightTicketApplication.class, args);
+    }
+
+}
---
+++ advanced_first_project/src/test/java/com/tenco/team_two_flight_ticket/TeamTwoFlightTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tenco.team_two_flight_ticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamTwoFlightTicketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
